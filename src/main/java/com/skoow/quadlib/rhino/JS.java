14b6e6983e4d9885e4e78a3b77dc638d69954b41
Skoow <89569841+VuzZis@users.noreply.github.com>
2024-12-16 18:06:59 +0500

    врапперы (вродь работают)

--- src/main/java/com/skoow/quadlib/rhino/JS.java
+++ src/main/java/com/skoow/quadlib/rhino/JS.java
@@ -3,8 +3,7 @@ package com.skoow.quadlib.rhino;
 import com.skoow.quadlib.utilities.struct.Pair;
 import com.skoow.quadlib.utilities.struct.Seq;
 import com.skoow.rhino.*;
-
-
+import com.skoow.rhino.util.wrap.TypeWrappers;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -13,7 +12,8 @@ import java.util.HashMap;
 public class JS {
     public static Pair<Context, ScriptableObject> begin() {
         Context context = Context.enter();
-        //todo: тайп врапперы?
+        TypeWrappers wraps = context.getTypeWrappers();
+        Wrappers.register(wraps);
         ScriptableObject scope = context.initStandardObjects(new TopLevel());
         return new Pair<>(context,scope);
     };
---
+++ src/main/java/com/skoow/quadlib/rhino/Wrappers.java
@@ -0,0 +1,94 @@
+package com.skoow.quadlib.rhino;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.skoow.quadlib.utilities.math.Vec3;
+import com.skoow.quadlib.utilities.struct.Pair;
+import com.skoow.quadlib.utilities.struct.Structs;
+import com.skoow.rhino.NativeArray;
+import com.skoow.rhino.util.wrap.TypeWrappers;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.TagParser;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Wrappers {
+    public static void register(TypeWrappers wraps) {
+        wraps.register(Vec3.class,(ctx, obj) -> {
+            if(obj instanceof Vec3 vec) return vec;
+            if(obj instanceof Entity e) return Vec3.from(e);
+            if(obj instanceof NativeArray d) return new Vec3((float) (double) (Double) d.get(0),(float) (double) (Double) d.get(1),(float) (double) (Double) d.get(2));
+            return null;
+        });
+        wraps.register(Component.class,(ctx, obj) -> {
+            if(obj instanceof Component c) return c;
+            if(obj instanceof String a) return Component.translatable(a);
+            return Component.literal(String.valueOf(obj));
+        });
+        wraps.register(UUID.class,(ctx, obj) -> obj == null ? null : UUID.fromString(String.valueOf(obj)));
+        wraps.register(BlockPos.class,(ctx, obj) -> {
+            if(obj instanceof BlockPos pos) return pos;
+            if(obj instanceof Vec3 vec) return BlockPos.containing(vec.x,vec.y,vec.z);
+            if(obj instanceof Entity e) return e.getOnPos();
+            if(obj instanceof NativeArray d) return BlockPos.containing((Double) d.get(0),(Double) d.get(1),(Double) d.get(2));
+            return null;
+        });
+        wraps.register(ItemStack.class,(ctx, obj) -> {
+            if(obj instanceof ItemStack stack) return stack;
+            if(obj instanceof String str) {
+                if(ForgeRegistries.ITEMS.getValue(ResourceLocation.of(str, ':')) == null) return null;
+                return new ItemStack(ForgeRegistries.ITEMS.getValue(ResourceLocation.of(str, ':')));
+            }
+            if(obj instanceof Pair pair && pair.first() instanceof String s && pair.second() instanceof Integer i) {
+                if(ForgeRegistries.ITEMS.getValue(ResourceLocation.of(s, ':')) == null) return null;
+                return new ItemStack(ForgeRegistries.ITEMS.getValue(ResourceLocation.of(s, ':')),i);
+            }
+            return null;
+        });
+        wraps.register(Item.class,(ctx,obj) -> {
+            if(obj instanceof Item i) return i;
+            if(obj instanceof String str)
+                return ForgeRegistries.ITEMS.getValue(ResourceLocation.of(str, ':'));
+            return null;
+        });
+        wraps.register(Block.class,(ctx, obj) -> {
+            if(obj instanceof Block b) return b;
+            if(obj instanceof String str)
+                return ForgeRegistries.BLOCKS.getValue(ResourceLocation.of(str, ':'));
+            return null;
+        });
+        wraps.register(ResourceLocation.class,(ctx,obj) -> {
+            if(obj instanceof ResourceLocation res) return res;
+            if(obj instanceof String str) return new ResourceLocation(str);
+            if(obj instanceof Pair p && p.first() instanceof String s1 && p.second() instanceof String s2)
+                return new ResourceLocation(s1,s2);
+            return null;
+        });
+        wraps.register(CompoundTag.class,(ctx, obj) -> {
+            if (obj instanceof CompoundTag nbt) return nbt;
+            else if (obj instanceof CharSequence c) try {
+                    return TagParser.parseTag(c.toString());
+                } catch (Exception ex) {
+                    return null;
+                }
+            HashMap<String,Object> tag = Structs.map(String.class,Object.class,(Object[]) obj);
+            CompoundTag nbt = new CompoundTag();
+            tag.forEach((k,v) -> {
+                try {
+                    nbt.put(k, TagParser.parseTag(String.valueOf(v)));
+                } catch (CommandSyntaxException ignored) {
+
+                }
+            });
+            return nbt;
+        });
+    }
+}
